1d3b5598fb9927b9018ecbbca227d4dfe2405c2f
Owen Cross <owencross99@gmail.com>
2025-12-06 17:45:59 +0100

    basic validation

--- Src/Aidn.NewsScore.Api/Endpoints/NewsScores/CreateNewsScore/CreateNewsScoreRequestValidator.cs
+++ Src/Aidn.NewsScore.Api/Endpoints/NewsScores/CreateNewsScore/CreateNewsScoreRequestValidator.cs
@@ -1,6 +1,75 @@
+using Aidn.NewsScore.Api.Validation;
+using FastEndpoints;
+using FluentValidation;
+
 namespace Aidn.NewsScore.Api.Endpoints.NewsScores.CreateNewsScore;
 
-public class CreateNewsScoreRequestValidator
+public class CreateNewsScoreRequestValidator : Validator<CreateNewsScoreRequest>
 {
-    
+    private static readonly HashSet<string> _requiredMeasurementTypes = ["TEMP", "HR", "RR"];
+    private static readonly string _measurementTypesListMessage =
+        $"must contain exactly one of each of the following: {_requiredMeasurementTypes.ToValidationMessageFormat()}";
+
+    public CreateNewsScoreRequestValidator()
+    {
+        RuleFor(x => x.Measurements)
+            .Must(HaveNoMissingTypes)
+            .WithMessage(request =>
+            {
+                var providedTypes = request.Measurements.Select(m => m.Type).Distinct().ToHashSet();
+                var missingTypes = _requiredMeasurementTypes.Where(required => !providedTypes.Contains(required));
+                return $"'Measurements' {_measurementTypesListMessage}. Missing type(s): {missingTypes.ToValidationMessageFormat()}";
+            });
+
+        RuleFor(x => x.Measurements)
+            .Must(HaveNoDuplicateTypes)
+            .WithMessage(request =>
+            {
+                var duplicateTypes = request.Measurements.GroupBy(m => m.Type).Where(g => g.Count() > 1).Select(g => g.Key);
+                return $"'Measurements' {_measurementTypesListMessage}. Duplicate type(s): {duplicateTypes.ToValidationMessageFormat()}";
+            });
+
+        RuleFor(x => x.Measurements)
+            .Must(HaveNoUnexpectedTypes)
+            .WithMessage(request =>
+            {
+                var providedTypes = request.Measurements.Select(m => m.Type).Distinct();
+                var unexpectedTypes = providedTypes.Except(_requiredMeasurementTypes);
+                return $"'Measurements' {_measurementTypesListMessage}. Unknown type(s): {unexpectedTypes.ToValidationMessageFormat()}";
+            });
+
+        RuleForEach(x => x.Measurements)
+            .ChildRules(measurement =>
+            {
+                measurement
+                    .RuleFor(m => m.Type)
+                    .Must(BeValidMeasurementType)
+                    .WithMessage(m =>
+                        $"Measurement type must be one of: {_requiredMeasurementTypes.ToValidationMessageFormat()}. You input '{m.Type}'"
+                    );
+            });
+    }
+
+    private bool HaveNoMissingTypes(IEnumerable<Measurement> measurements)
+    {
+        var providedTypes = measurements.Select(m => m.Type).Distinct().ToHashSet();
+        return _requiredMeasurementTypes.All(required => providedTypes.Contains(required));
+    }
+
+    private bool HaveNoDuplicateTypes(IEnumerable<Measurement> measurements)
+    {
+        var types = measurements.Select(m => m.Type).ToList();
+        return types.Count == types.Distinct().Count();
+    }
+
+    private bool HaveNoUnexpectedTypes(IEnumerable<Measurement> measurements)
+    {
+        var providedTypes = measurements.Select(m => m.Type).Distinct();
+        return !providedTypes.Except(_requiredMeasurementTypes).Any();
+    }
+
+    private static bool BeValidMeasurementType(string type)
+    {
+        return _requiredMeasurementTypes.Contains(type);
+    }
 }
---
+++ Src/Aidn.NewsScore.Api/Validation/ValidationExtensions.cs
@@ -0,0 +1,12 @@
+namespace Aidn.NewsScore.Api.Validation;
+
+public static class ValidationExtensions
+{
+    extension<T>(IEnumerable<T> enumerable)
+    {
+        public string ToValidationMessageFormat()
+        {
+            return string.Join(", ", enumerable.Select(t => $"'{t}'"));
+        }
+    }
+}
